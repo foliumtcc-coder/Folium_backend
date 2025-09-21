e1f35e2553854237969c92882129a375e071a89f
LuizFelipeCalcagno <folium.tcc@gmail.com>
2025-09-21 01:13:27 -0300

    Update user.js

--- routes/auth/user.js
+++ routes/auth/user.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
-// GET /api/user/me
 router.get('/me', (req, res) => {
-  if (!req.session.user_id) return res.status(401).json({ user: null });
-
-  res.json({ user: { id: req.session.user_id, nome: req.session.user_nome } });
+  if (req.session.user) {
+    return res.json({ user: req.session.user });
+  } else {
+    return res.json({ user: null });
+  }
 });
 
 export default router;
