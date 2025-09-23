738f7fb607dfae6c050d163993bb4e97ab7bdbf2
LuizFelipeCalcagno <folium.tcc@gmail.com>
2025-09-23 20:47:20 -0300

    Update projectsView.js

--- routes/auth/projectsView.js
+++ routes/auth/projectsView.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getPublicProjects, getPrivateProjects, getAllProjects } from '../../controllers/projectViewController.js';
+import { getPublicProjects, getPrivateProjects, getAllProjects, getUserProjects } from '../../controllers/projectViewController.js';
 import { authenticate } from '../../utils/authenticate.js';
 
 const router = express.Router();
@@ -7,5 +7,8 @@ const router = express.Router();
 router.get('/public', getPublicProjects);
 router.get('/private', authenticate, getPrivateProjects);
 router.get('/all', authenticate, getAllProjects);
+
+// NOVA ROTA para projetos de um usuário específico
+router.get('/user/:id', authenticate, getUserProjects);
 
 export default router;
